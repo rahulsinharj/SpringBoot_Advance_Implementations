e20f37451f71d9d6ba645d32684d4f4de2f06e64
rahulsinharj <rahulkumarsinha1234@gmail.com>
2023-07-26 22:47:55 +0530

    spring

--- Sboot_GraphQL/src/main/java/com/controller/BookController.java
+++ Sboot_GraphQL/src/main/java/com/controller/BookController.java
@@ -58,6 +58,38 @@ class BookInput
 	private String author;
 	private double price;
 	private int pages;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDesciption() {
+		return desciption;
+	}
+	public void setDesciption(String desciption) {
+		this.desciption = desciption;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getPages() {
+		return pages;
+	}
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+	
+	
 }
 
 	
